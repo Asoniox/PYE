1e2d9a348486e4adeeae317ab5e77d6536f938d7
TimelessTTV <timeless.off.tv@gmail.com>
2022-09-27 18:53:13 +0300

    Initial commit

---
+++ squarearea.py
@@ -0,0 +1,5 @@
+x = float(input("Enter length in cm: "))
+y = float(input("Enter width in cm: "))
+mu = str(input("Enter measuring unit: "))
+area = x*y
+print("Area of square is:", area, "squared", mu)
