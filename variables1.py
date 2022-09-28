4711bdf534d6657bba0d5373ea25ed1d683d8d4c
Timeless <85189523+TimelessOn15Hz@users.noreply.github.com>
2022-09-28 11:08:16 +0300

    Minor additions

--- variables1.py
+++ variables1.py
@@ -4,3 +4,10 @@ x=45 #Initial value
 x=45.5 #Value changes from 45 to 45.5
 print x #Prints the value (Note: It prints the most recent value set!)
 print x+x #Calculation: 45.5 + 45.5
+
+"""
+Running the code:
+>>>python variables1.py
+45.5
+91.0
+"""
