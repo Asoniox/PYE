97d3d2ac0ec77829356b190af464a26e0861a171
TimelessOn15Hz <timeless.off.tv@gmail.com>
2023-09-18 18:16:51 +0300

    Create playground file

---
+++ playground.py
@@ -0,0 +1,12 @@
+"""
+Playground
+-------------
+Testing playground for PYEs and related modules
+"""
+import sqrarea
+
+a = float(input("Square Height: "))
+b = float(input("Square Width: "))
+
+area = sqrarea.sqrarea(a, b)
+print "Square Area:", area
