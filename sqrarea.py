d742f2995d85045ba6b14fc20ff9696487f6a599
TimelessOn15Hz <timeless.off.tv@gmail.com>
2023-09-18 18:17:10 +0300

    Rename & rewrite squarearea.py

---
+++ sqrarea.py
@@ -0,0 +1,25 @@
+"""
+sqrarea
+-------------
+Compute the area of a square/rectangle.
+"""
+
+def sqrarea(length, width):
+    """
+    Compute the area of a square/rectangle.
+
+    Parameters
+    ----------
+    length: `float`
+        Length of square/rectangle in centimeters.
+
+    width: `float`
+        Width of square/rectangle in centimeters.
+
+    Returns
+    -------
+    area: `float`
+        Area of square/rectangle in centimeters.
+    """
+    area = length*width
+    return area
--- squarearea.py
+++
@@ -1,7 +0,0 @@
-"""Finds the area of a square!"""
-
-x = float(input("Enter length in cm: "))
-y = float(input("Enter width in cm: "))
-mu = str(input("Enter measuring unit: "))
-area = x*y
-print("Area of square is:", area, mu)
